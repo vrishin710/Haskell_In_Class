a33ca8d125311c3df80ca2650b7ed05079ba3e84
vrishin710 <vrishinmedapati@gmail.com>
2024-12-12 09:59:45 +0000

    Update palindrome.hs

--- palindrome.hs
+++ palindrome.hs
@@ -1,1 +1,13 @@
+removeSpace :: String -> String 
+removeSpace [] = []
+removeSpaces (c:cs)
+|c /=  ' ' = c:removeSpace cs 
+|otherwise = removeSpace cs
+ 
+reverse' :: String -> String
+reverse' [] = []
+reverse' (c:cs) = reverse' vs ++ [c]
+
+isPalindrome :: String -> Bool
+isPalindrome cs = reverse' cs == cs
 
